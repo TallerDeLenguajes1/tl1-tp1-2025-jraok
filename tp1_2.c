5620412200a06b870bf44d3c15aa5d8f6c096886
Joel Román Arancibia <joelromanarancibia@gmail.com>
2025-03-27 00:23:00 -0300

    ejercicio 4b completado

--- tp1_2.c
+++ tp1_2.c
@@ -2,15 +2,36 @@
 #include <math.h>
 
 float cuadrado(float numero);
+void cuadradoVoid();
 
 int main(){
 
     float numero;
-    
-    printf("\nIngrese un numero: ");
-    scanf("%f", &numero);
-    printf("\nEl cuadrado del numero es: %.2f", cuadrado(numero));
+    int op=0;
 
+    do{
+        puts("\n\t\t---Seleccione una funcion para utilizar---");
+        puts("\n1-\tCuadrado");
+        puts("\n2-\tCuadradoVoid");
+        puts("\n1-\tSalir");
+        scanf("%d", &op);
+        switch (op){
+        case 1:
+            printf("\nIngrese un numero: ");
+            scanf("%f", &numero);
+            printf("\nEl cuadrado del numero es: %.2f", cuadrado(numero));
+            break;
+        
+        case 2:
+            cuadradoVoid();
+            break;
+
+        default:
+            break;
+        }
+        
+    } while (op<1 || op>3);
+       
     return 0;
 }
 
@@ -18,4 +39,10 @@ float cuadrado(float numero){
     return(numero*numero);
 }
 
-
+void cuadradoVoid(){
+    float numero;
+    
+    printf("\nIngrese un numero: ");
+    scanf("%f", &numero);
+    printf("\nEl cuadrado del numero es: %.2f", numero*numero);
+} 
