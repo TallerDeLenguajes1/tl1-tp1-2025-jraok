1a433e874cdbd89254f9ff0f64892023a589b311
Joel Román Arancibia <joelromanarancibia@gmail.com>
2025-03-27 01:02:06 -0300

    4c completado y nueva regla en gitignore

--- tp1_2.c
+++ tp1_2.c
@@ -3,6 +3,7 @@
 
 float cuadrado(float numero);
 void cuadradoVoid();
+void direccionDeMemoria();
 
 int main(){
 
@@ -11,26 +12,33 @@ int main(){
 
     do{
         puts("\n\t\t---Seleccione una funcion para utilizar---");
-        puts("\n1-\tCuadrado");
-        puts("\n2-\tCuadradoVoid");
-        puts("\n3-\tSalir");
+        puts("1-\tCuadrado");
+        puts("2-\tCuadradoVoid");
+        puts("3-\tDireccion de memoria de una variable");
+        puts("4-\tSalir");
         scanf("%d", &op);
         switch (op){
         case 1:
             printf("\nIngrese un numero: ");
             scanf("%f", &numero);
-            printf("\nEl cuadrado del numero es: %.2f", cuadrado(numero));
+            printf("\nEl cuadrado del numero es: %.2f\n", cuadrado(numero));
             break;
         
         case 2:
             cuadradoVoid();
             break;
+            
+        case 3:
+            direccionDeMemoria();
+            break;
 
         default:
             break;
         }
-        
-    } while (op<1 || op>3);
+        if (op<1 || op>4){
+            printf("\nopcion invalida");
+        }
+    } while (op != 4);
        
     return 0;
 }
@@ -41,8 +49,39 @@ float cuadrado(float numero){
 
 void cuadradoVoid(){
     float numero;
-    
+    fflush(stdin);
     printf("\nIngrese un numero: ");
     scanf("%f", &numero);
-    printf("\nEl cuadrado del numero es: %.2f", numero*numero);
+    printf("\nEl cuadrado del numero es: %.2f\n", numero*numero);
 } 
+
+void direccionDeMemoria(){
+    
+    int opcion=0;
+    do{
+        puts("\n\n\t\t--Seleccione el tipo de variable--");
+        puts("1-\tCaracter");
+        puts("2-\tNumerica");
+        puts("3-\tSalir");
+        scanf("%d",&opcion);
+        fflush(stdin);
+        if (opcion==1){
+            char caracter;
+            printf("\nIngrese un unico caracter: ");
+            scanf("%c",&caracter);
+            printf("Direccion de memoria de la variable: %p\n", &caracter);
+            printf("Contenido de la variable: %c\n", caracter);
+        }
+        if (opcion==2){
+            float num;
+            printf("\nIngrese un numero: ");
+            scanf("%f", &num);
+            printf("Direccion de memoria de la variable: %p\n", &num);
+            printf("Contenido de la variable: %.2f\n", num);
+        }
+        if (opcion < 1 || opcion>3){
+            printf("\nopcion invalida");
+        }
+        
+    } while (opcion != 3);
+}
