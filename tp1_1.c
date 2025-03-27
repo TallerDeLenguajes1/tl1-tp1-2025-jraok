f17de09d14ed0a4425c614b9fd914f035e2323ae
Joel Román Arancibia <joelromanarancibia@gmail.com>
2025-03-26 21:08:30 -0300

    realicé el ejercicio '3e'

--- tp1_1.c
+++ tp1_1.c
@@ -2,7 +2,16 @@
 
 int main(){
 
-    printf("\nHola Mundo");
+    printf("\nHola Mundo\n");
+
+    int variable = 1;
+    int *puntero = &variable;
+
+    printf("\n1-\tContenido del punteto: %d", *puntero);
+    printf("\n2-\tDireccion de memoria almacenada en el puntero puntero: %p", puntero);
+    printf("\n3-\tDireccion de memoria de la variable: %p", &variable);
+    printf("\n4-\tDireccion de memoria del puntero: %p", &puntero);
+    printf("\n5-\tTamanio de memoria usado por la variable: %lu bytes\n", sizeof(variable));
 
     return 0;
 }
